d773fefa90ec3b2f7dd6bffb9c7dfe7bfa78a678
Drew <33384698+Ahendrix9624@users.noreply.github.com>
2023-03-11 14:40:08 -0700

    Update not_ransomware.py

--- not_ransomware.py
+++ not_ransomware.py
@@ -1,32 +1,13 @@
-#!/usr/bin/env python3
-#
-#  [Program]
-#
-#  Not Ransomeware
-#
-#  [Author]
-#
-#  Drew, https://github.com/Ahendrix9624/
-#
-#  [License]
-#
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 3 of the License, or
-#  any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-#
-#  See 'LICENSE' for more information.
-
-
+"""
+USAGE - The code encrypts files in the current directory using the Fernet encryption algorithm. 
+        It first finds all files in the directory, except for certain files (not_ransomware, 
+        thekey.key, decrypt.py), generates a unique encryption key using the Fernet library, 
+        writes the key to a file called "thekey.key", encrypts each file using the key, 
+        and overwrites the original file with the encrypted contents. The program then 
+        prints a message threatening to delete the files unless 100 billion dollars are paid in Bitcoin.
+        
+AUTHOR - https://github.com/Ahendrix9624/
+"""
 
 import os
 from cryptography.fernet import Fernet
